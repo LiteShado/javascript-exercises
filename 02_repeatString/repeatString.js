9235bac176653716b4c8b690d01be0a18ed08c8b
Natalie <theliteshado@icloud.com>
2021-12-02 22:12:21 -0500

    basics

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (w, y) {
+  if (y < 0) return "ERROR";
 
+  let theWord = "";
+
+  for (x = 0; x < y; x++) {
+    theWord += w;
+  }
+  return theWord;
 };
 
 // Do not edit below this line
