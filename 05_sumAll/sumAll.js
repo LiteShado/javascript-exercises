f68142af943dc531a4209955c507984848094b59
Natalie <theliteshado@icloud.com>
2021-12-02 22:25:32 -0500

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (f, y) {
+  if (!Number.isInteger(f) || !Number.isInteger(y)) return "ERROR";
+  if (f < 0 || y < 0) return "ERROR";
 
+  if (f > y) {
+    const temp = f;
+    f = y;
+    y = temp;
+  }
+
+  let sum = 0;
+  for (x = 0; x < y + 1; x++) {
+    sum += x;
+  }
+  return sum;
 };
 
 // Do not edit below this line
